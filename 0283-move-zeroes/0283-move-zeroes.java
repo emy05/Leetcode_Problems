9520a0294b8fac90026579122f4054505482d73c
Emy Premlin <97127324+emy05@users.noreply.github.com>
2025-08-23 16:18:14 +0530

    Time: 2 ms (79.85%), Space: 46.7 MB (6.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,29 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        if (n == 0){
-            return;
-        }
-
-        int j = -1;
-        for(int i=0; i<n; i++){
-            if (nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-
-        if (j==-1){
-            return;
-        }
-
-        for(int i=j+1; i<n; i++){
-            if (nums[i]!=0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-        }
+       int zerosCount = 0;
+       for(int i=0;i<nums.length;i++){
+         if(nums[i] == 0){
+            zerosCount++;
+         } else if(zerosCount > 0) {
+            nums[i-zerosCount] = nums[i];
+            nums[i]=0;
+         }
+       }
     }
 }
