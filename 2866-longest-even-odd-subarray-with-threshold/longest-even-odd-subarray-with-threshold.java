267da6e59f02196d7716a28435a5e5cecd5d2c27
Emy Premlin <97127324+emy05@users.noreply.github.com>
2026-01-17 23:29:41 +0530

    Time: 4 ms (98.54%) | Memory: 47.1 MB (30.66%) - LeetSync

--- 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.java
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.java
@@ -2,22 +2,22 @@ class Solution {
     public int longestAlternatingSubarray(int[] nums, int threshold) {
         int maxLen = 0;
         int left = 0;
-        int n = nums.length;
-        for(int right = 0; right<n; right++){
+
+        for(int right = 0; right < nums.length; right++){
             if(nums[right] > threshold){
-                left = right + 1;
+                left = right + 1; // right is invalid so skip
+                continue;
             }
 
             if(right > left && nums[right] % 2 == nums[right-1] % 2){
                 left = right;
             }
 
-            if(left < n && nums[left] % 2 != 0){
-                left = right + 1;
+            if(nums[left] % 2 != 0){
+                left = right + 1; //restart the window
                 continue;
             }
-
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxLen = Math.max(maxLen, right-left+1);
         }
         return maxLen;
     }
